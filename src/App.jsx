02cbdf80e433b7c3507bfd7b359f80a018bb4137
Sujay Chowdhury <your_github_email@example.com>
2026-03-07 22:17:11 +0530

    make ecommerce project  standard folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold underline">
         Welcome to ecommerce vegetables store
       </h1>
     </>
